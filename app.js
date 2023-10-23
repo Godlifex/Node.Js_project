af675b11026f0b7995eea9349ffd913b6485596a
Godlifex <prediger.denas@gmail.com>
2023-10-23 22:08:17 +0300

    final

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-import express from 'express';
+const express = ('express');
+const faker = ('faker');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get('/random-users', (req, res) => {
+  const numberOfUsers = 5; // Number of random users to generate
+
+  const randomUsers = [];
+
+  for (let i = 0; i < numberOfUsers; i++) {
+    const user = {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+    };
+
+    randomUsers.push(user);
+  }
+
+  res.json(randomUsers);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
